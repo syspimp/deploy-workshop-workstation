7d3a9f110db330ae9127e2f0fcd78a1b19db7071
David Taylor <dataylor@yogac940.tfound.org>
2021-08-19 17:03:59 -0400

    disabling verbose ansible play

--- rerun-ansible.sh
+++ rerun-ansible.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#ansible-playbook -vvv --vault-password-file ./vault_secret launch-workshop-workstation.yml
 ansible-playbook --vault-password-file ./vault_secret launch-workshop-workstation.yml
